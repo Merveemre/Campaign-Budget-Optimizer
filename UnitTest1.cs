b937dc6590062065f634560500ab2bcbcd942ca9
Merve Emre <129984815+Merveemre@users.noreply.github.com>
2024-08-01 11:19:02 +0200

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace CampaignBudgetOptimizer.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
